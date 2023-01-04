a8c585a117ade095dfb314dbc474b9c7855339e0
MRodrigo1 <76001389+MRodrigo1@users.noreply.github.com>
2023-01-04 23:56:06 +0000

    up

--- TP_Pweb/Models/Reserva.cs
+++ TP_Pweb/Models/Reserva.cs
@@ -12,6 +12,7 @@
         public State state { get; set; }
         public DateTime DataEntrega { get; set; }
         public DateTime DataRecolha { get; set; }
+        //TODO apagar veiculo apaga reserva tratar
         public string UtilizadorId { get; set; }
         public Utilizador Utilizador { get; set; }
         public int VeiculoId { get; set; }
